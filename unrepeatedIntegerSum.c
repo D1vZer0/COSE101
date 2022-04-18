d0883960248a8a732f66b15dd5a01c06ec5729a5
Kyung-ik Lee <aa20822@naver.com>
2022-04-18 13:30:39 +0900

    Update unrepeatedIntegerSum.c

--- unrepeatedIntegerSum.c
+++ unrepeatedIntegerSum.c
@@ -2,18 +2,20 @@
 
 int main ()
 {
-    int n[100], i, j, flag=0, sum=0;
-    for (i=0; i<100; i++) {
+    int n[100], i=0, j=0, flag=1, sum=0;
+    
+    while (flag) {
         scanf ("%d", &n[i]);
-        for (j=0; j<i; j++) {
-            if (n[i] == n[j]) {
-                flag=1;
+        
+        for (j=0; j<i; j++) { //flip the flag if repeated integer detected
+            if (n[i] == n[j]) { 
+                flag = 0;
             }
         }
-        if (flag) {
-            break;
+        if (flag) { //if not detected, add the input
+            sum += n[i];
         }
-        sum += n[i];
+        i++;
     }
     printf ("%d", sum);
     
