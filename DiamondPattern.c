f3305d14fd1816d90b6874f143fd78b6de3c9f3c
Kyung-ik Lee <aa20822@naver.com>
2022-04-20 12:33:09 +0900

    Create DiamondPattern.c

---
+++ DiamondPattern.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, k, m=1;
+    scanf ("%d", &n);
+    k=n-1;
+    for (i=0; i<n; i++) {
+        for (j=0; j<k; j++) {
+            printf (" ");
+        }
+        for (m=n-k; m>0; m--) {
+            printf ("%d", m);
+        }
+        if (n-k != 1) {
+            for (m=2; m<=n-k; m++) {
+                printf ("%d", m);
+            }
+        }
+        printf ("\n");
+        k--;
+    }
+    k=1;
+    for (i=0; i<n-1; i++) {
+        for (j=0; j<k; j++) {
+            printf (" ");
+        }
+        for (m=n-k; m>0; m--) {
+            printf ("%d", m);
+        }
+        if (n-k != 1) {
+            for (m=2; m<=n-k; m++) {
+                printf ("%d", m);
+            }
+        }
+        printf ("\n");
+        k++;
+    }
+    
+    return 0;
+}
